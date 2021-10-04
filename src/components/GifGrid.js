8e87ff40adad23850aa804cea8f059effa7907ba
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-10-04 04:21:02 -0400

    Implemented useEffect to prevent use more than one time the getGifs function

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export const GifGrid = ({category}) => {
     // Necesito recibir la categoria y llamar la peticion http 
+
+    const [counter, setCounter] = useState(0);
+    useEffect(() => {
+        getGifs();
+    }, []); //cuando se pone el [] , ecita que se ejecute mas de una vez 
 
     const getGifs = async () => {
         const url = `https://api.giphy.com/v1/gifs/search?api_key=Nr4bfrgRK7obHM3O68YwvQ2iUmuxi8Yj&q=Rick+and+Morty&limit=10`;
@@ -22,12 +27,14 @@ export const GifGrid = ({category}) => {
          
     }
 
-    getGifs();
+    // getGifs();
 
     return (
         <>
+            <h2>{counter}</h2>
             {/* <h3>{category}</h3> */}
             <li key={category}>{category}</li>
+            <button onClick={() => {setCounter(c => c + 1)}}>Count</button>
         </>
     )
 }
