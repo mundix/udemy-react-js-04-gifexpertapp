565788859f68080f3c8d3bcd482aef8fcd498ac6
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-10-06 06:03:49 -0400

    Tests on  GifGridItem

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -2,16 +2,39 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { GifGridItem } from '../../components/GifGridItem';
 
-
 describe('Pruebas en archivo GifGridItem', () => {
 
    const title = 'Un titulo';
    const url = 'https://localhost/algo.js';
+   const wrapper = shallow(<GifGridItem title={title} url={url}/>);
     
    test('Comprobar GifGridItem correctamente', () => {
-       
-        const wrapper = shallow(<GifGridItem title={title} url={url}/>);
         expect(wrapper).toMatchSnapshot();
-   })
-    
+   });
+
+   test('Dene tener un parrafo con el title', () => {
+      const p = wrapper.find('p');
+      expect(p.text().trim()).toBe(title);
+   });
+
+   test('Debe tener la la imagen igual al url y alt de los props', () => {
+      const img = wrapper.find('img');
+      // console.log(img.props());
+      // console.log(img.prop('src'));
+
+      expect(img.prop('src')).toBe(url);
+      expect(img.prop('alt')).toBe(title);
+   });
+
+   test('Debe de tener animate_fadeIn', () => {
+      const div = wrapper.find('div');
+      expect(div.hasClass('animate__fadeIn')).toBe(true);
+   });
+
+   test('Debe de tener animate_fadeIn con Includes', () => {
+      const div = wrapper.find('div');
+      const className = div.prop('className');
+      expect(className.includes('animate__fadeIn')).not.toBe(false);
+   });
+   
 });
