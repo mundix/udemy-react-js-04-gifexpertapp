84affdac2f86d68847d6a4f2654890cd25a8e2f8
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-10-07 14:12:14 -0400

    Testing GifGrid

--- src/tests/components/GifGrid.test.js
+++ src/tests/components/GifGrid.test.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { GifGrid } from '../../components/GifGrid';
+import { useFetchGifs } from '../../hooks/useFetchGifs';
+//Fingir cualquier llamada a ese archivo y controlar la info de respuesta
+jest.mock('../../hooks/useFetchGifs'); 
 
 describe('Prueas en el GifGrid', () => {
-    
+
     const category = 'Pokemon';
-    const wrapper = shallow(<GifGrid category={category}/>);
 
     test('Mostrar correctamente GifGrid', () => {
-       
+
+        // para hacer una falsa impklementacion  
+        useFetchGifs.mockReturnValue({
+            data: [],
+            loading: true
+        });
+
+        const wrapper = shallow(<GifGrid category={category} />);
         expect(wrapper).toMatchSnapshot();
 
     });
-    
+
+    test('Debe mostra items cuando se cargan imagenes useFetchGifs', () => {
+
+        const gifs = [{
+            id: 'ABC',
+            url: 'https://localhost/cualqueira.jpg',
+            title: 'cualquier cosa'
+        }];
+
+        useFetchGifs.mockReturnValue({
+            data: gifs,
+            loading: false
+        });
+
+        const wrapper = shallow(<GifGrid category={category} />);
+
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('p').exists()).toBe(false);
+        expect(wrapper.find('GifGridItem').length).toBe(gifs.length); //si existe 
+
+         
+    });
+
 
 });
